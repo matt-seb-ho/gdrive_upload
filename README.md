c1da5676d01449d6c1450641a824d30484f37cea
Matthew Ho <matt.seb.ho@gmail.com>
2024-08-08 21:11:13 -0700

    mvp

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # gdrive_upload
+a tiny one file wrapper over google drive API to upload files into a google drive
+
+## Setup Instructions
+### Step 1: Install Required Libraries
+First, you'll need to install the `google-auth`, `google-auth-oauthlib`, `google-auth-httplib2`, and `google-api-python-client` libraries.
+
+You can install them using pip:
+
+```bash
+pip install google-auth google-auth-oauthlib google-auth-httplib2 google-api-python-client
+```
+
+### Step 2: Create a Google Cloud Project and Enable the Drive API
+
+1. Go to the [Google Cloud Console](https://console.cloud.google.com/).
+2. Create a new project.
+3. Once the project is created, go to the **API & Services > Library** section.
+4. Search for "Google Drive API" and click on it.
+5. Click the "Enable" button to enable the API for your project.
+6. Go to **Credentials** on the left-hand menu, then click on "Create Credentials" and choose "OAuth 2.0 Client IDs".
+7. Set up the OAuth consent screen (you can set it up as an internal app if you're just testing).
+8. Create the OAuth 2.0 client ID, and choose "Desktop app" as the application type.
+9. Download the JSON file with your credentials. Rename it to `credentials.json` and place it in your working directory.
+
+#### (Optional) Step 2a: Set up a Service Account
+1. **Create a Service Account:**
+   - Go to the [Google Cloud Console](https://console.cloud.google.com/).
+   - Navigate to **IAM & Admin > Service Accounts**.
+   - Click "Create Service Account".
+   - After creating, navigate to the service account, and click "Add Key" > "Create new key" > Select JSON format.
+   - Download the key file. This is the correct `credentials.json` file for a service account.
+
+2. **Use the Correct Script:**
+   - TODO: Currently working on script to make this an option
+
+### Step 3: Run the Script:
+- When running for the first time, the script will open a browser window for you to authenticate with your Google account.
+- After authentication, it will save a token.json file in your working directory, which will be used for subsequent runs.
---
+++ upload_file_to_drive.py
@@ -0,0 +1,107 @@
+import argparse
+import os
+from pathlib import Path
+
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
+from google.auth.transport.requests import Request
+
+# If modifying these SCOPES, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.file']
+
+
+def authenticate():
+    creds = None
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+    return creds
+
+
+def get_folder_id(service, folder_name, parent_folder_id=None):
+    # Search for the folder in the current parent directory
+    query = f"name = '{folder_name}' and mimeType = 'application/vnd.google-apps.folder'"
+    if parent_folder_id:
+        query += f" and '{parent_folder_id}' in parents"
+    
+    results = service.files().list(
+        q=query,
+        spaces='drive',
+        fields="files(id, name)",
+        pageSize=10
+    ).execute()
+
+    items = results.get('files', [])
+    if items:
+        # If the folder exists, return its ID
+        return items[0]['id']
+    else:
+        # If the folder doesn't exist, create it
+        folder_metadata = {
+            'name': folder_name,
+            'mimeType': 'application/vnd.google-apps.folder'
+        }
+        if parent_folder_id:
+            folder_metadata['parents'] = [parent_folder_id]
+        folder = service.files().create(
+            body=folder_metadata,
+            fields='id'
+        ).execute()
+        return folder['id']
+
+
+def traverse_and_create_folders(service, path):
+    folders = path.split('/')
+    parent_id = None
+
+    for folder in folders[:-1]:  # Exclude the last part (the file name)
+        parent_id = get_folder_id(service, folder, parent_id)
+
+    return parent_id
+
+
+def upload_file_to_drive(file_path, destination_path):
+    creds = authenticate()
+    service = build('drive', 'v3', credentials=creds)
+    
+    # Traverse or create folders
+    parent_folder_id = traverse_and_create_folders(service, destination_path)
+    
+    # Extract the file name from the destination path
+    file_name = destination_path.split('/')[-1]
+    
+    # Upload the file
+    file_metadata = {'name': file_name}
+    if parent_folder_id:
+        file_metadata['parents'] = [parent_folder_id]
+
+    media = MediaFileUpload(file_path, resumable=True)
+
+    file = service.files().create(
+        body=file_metadata,
+        media_body=media,
+        fields='id'
+    ).execute()
+
+    file_id = file.get('id')
+    return file_id
+
+
+if __name__ == "__main__":
+    psr = argparse.ArgumentParser()
+    psr.add_argument('file_path', type=str, help='Path to the file to upload')
+    psr.add_argument('destination_path', type=str, help='Destination path in Google Drive')
+    args = psr.parse_args()
+
+    upload_file_to_drive(args.file_path, args.destination_path)
